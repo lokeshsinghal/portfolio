0cca967d875976640eeb03530ec29d3f71dede2d
Lokesh Singal <lokesh.singal@thoughts2binary.com>
2021-02-02 22:20:48 +0530

    Updates

---
+++ precache-manifest.a3133d519bced2556342570e77f71f0b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c831a69d48abc2b424c9bc8c0c75c3e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3b7f1a07b0a252aaa5a2",
+    "url": "/portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b2f1d5d57e5df99b1007",
+    "url": "/portfolio/static/css/main.53ee7ec1.chunk.css"
+  },
+  {
+    "revision": "3b7f1a07b0a252aaa5a2",
+    "url": "/portfolio/static/js/2.2aa4d15d.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/portfolio/static/js/2.2aa4d15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f1d5d57e5df99b1007",
+    "url": "/portfolio/static/js/main.9f5fe61a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9e79e2488519d9dc3a53c44712e1bf24",
+    "url": "/portfolio/static/media/Profile.9e79e248.jpg"
+  },
+  {
+    "revision": "83a17b64e7243d6bd3934e68ecc44e20",
+    "url": "/portfolio/static/media/Resume.83a17b64.pdf"
+  },
+  {
+    "revision": "47d4629389f0517ba142ff9bfc72e463",
+    "url": "/portfolio/static/media/Todo.47d46293.png"
+  },
+  {
+    "revision": "dc3f80b023618b699e2ad6b122bf9948",
+    "url": "/portfolio/static/media/movie.dc3f80b0.png"
+  },
+  {
+    "revision": "1c6976b89a63617e76f05399778dec03",
+    "url": "/portfolio/static/media/project.1c6976b8.jpg"
+  },
+  {
+    "revision": "4151125bdb6a7dbe62994d12cc01af4d",
+    "url": "/portfolio/static/media/t2b.4151125b.png"
+  }
+]);
