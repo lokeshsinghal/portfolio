877be39641fe8198ac3b57c48e39bb680d815001
Lokesh Singal <lokesh.singal@thoughts2binary.com>
2021-02-11 20:26:12 +0530

    dummy

--- src/Components/Particle.js
+++ src/Components/Particle.js
@@ -1,43 +1,7 @@
 import React from 'react';
-import Particles from 'react-particles-js';
 function Particle() {
     return (
-        <Particles id="particles-js"
-            params={{
-                particles: {
-                    number: {
-                        value: 400,
-                        density: {
-                            enable: true,
-                            value_area: 1000
-                        }
-                    },
-                    color: {
-                        value: '#fff'
-                    },
-                    opacity: {
-                        value: 0.5,
-                        anim: {
-                            enable: true
-                        }
-                    },
-                    size: {
-                        value: 7,
-                        random: true,
-                        anim: {
-                            enable: true,
-                            speed: 3
-                        }
-                    },
-                    line_linked: {
-                        enable: false
-                    },
-                    move: {
-                        speed: 0.2
-                    }
-                 }    
-            }}    
-        />
+        <div>Dummy</div>
     )
 }
 export default Particle;
